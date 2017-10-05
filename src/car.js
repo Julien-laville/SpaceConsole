774464d57b4d9bfd7abafa11c73b9d582e6b8ee6
Julien-laville <alternative451@myopera.com>
2017-10-05 21:41:41 +0200

    add pos form box2d

--- src/car.js
+++ src/car.js
@@ -1,3 +1,29 @@
-var car = function() {
-    
+const Car = function() {
+    let speed = v2d(0,0)
+    let acc = v2d(0,0)
+    let heading = 0
+    let pivotPos = v2d(0,0)
+
+    let carBox = planck.Box(20, 20);
+    let body = world.createDynamicBody(v2d(0,0))
+        .createFixture(carBox, 0.5);
+
+    return {
+        draw : function () {
+            ct.fillStyle = 'red'
+            ct.fillRect(body.getBody().getPosition().x,body.getBody().getPosition().y,20,20)
+        },
+        input : function () {
+            if(up) {
+                speed.y--
+            } else if(down) {
+                speed.y++
+            } else if(right) {
+                speed.x++
+            } else if(left) {
+                speed.x--
+            }
+            //pos.add(speed)
+        }
+    }
 }
