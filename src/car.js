e866ba502de12925d0c666f57369ef08bd220907
Julien-laville <alternative451@myopera.com>
2017-10-06 22:52:12 +0200

    lapin comrpis

--- src/car.js
+++ src/car.js
@@ -9,7 +9,7 @@ const Car = function() {
 
     let carBox = planck.Box(20, 20);
     let carFixture = world.createDynamicBody(v2d(20,20))
-        .createFixture(carBox, 1);
+        .createFixture(carBox)
     let body = carFixture.getBody()
     let pos = body.getPosition()
     let force
@@ -21,13 +21,20 @@ const Car = function() {
             ct.fillRect(pos.x,pos.y,20,20)
         },
         input : function () {
-            force =  1;
+            force =  0;
             if(up) {
+                document.title = 1
                 force = 1
+                body.applyForce(v2d(0,force), body.getWorldCenter())
             } else if(down) {
+                body.applyForce(v2d(0,force), body.getWorldCenter())
+
+                document.title = '-1'
                 force = -1
+            } else {
+                document.title = 'ha'
             }
-            body.applyForce(v2d(0,0.01).mul(1), body.getPosition())
+
         },
         r : function () {
             return body;
